cab02299bcb9c02bcb12ec064ba782c8329c9b8f
leesc22 <leesiewching22@gmail.com>
2018-07-05 21:23:39 +0800

    First commit

---
+++ app/src/main/java/com/github/leesc22/tourguideapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.leesc22.tourguideapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
